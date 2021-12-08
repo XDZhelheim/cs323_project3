cc3291e59f92eb0c4ed937642eb16f654055858f
XDZhelheim <601251915@qq.com>
2021-12-08 11:29:00 +0800

    init code_generator

---
+++ CodeGenerator.hpp
@@ -0,0 +1,33 @@
+#ifndef CODE_GENERTOR_HPP
+#define CODE_GENERTOR_HPP
+
+#include "NodeAnalyser.hpp"
+
+class Generator {
+private:
+    TreeNode *root;
+
+public:
+    Generator(TreeNode *node, string path)
+    {
+        root = node;
+        out = ofstream(path);
+    }
+
+    void generate() {
+        
+    }
+};
+
+void generateIntermidiateCode(char *file_path)
+{
+    string path = file_path;
+    string out_path = "/dev/stdout";
+    if (path.substr(path.length() - 4) == ".spl")
+    {
+        out_path = path.substr(0, path.length() - 4) + ".ir";
+    }
+    Generator(root, out_path).generate();
+}
+
+#endif
