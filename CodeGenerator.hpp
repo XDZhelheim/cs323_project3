fdce94d305370c260233dc07c29712afeea36425
XDZhelheim <601251915@qq.com>
2021-12-08 15:10:00 +0800

    init generator

--- CodeGenerator.hpp
+++ CodeGenerator.hpp
@@ -2,6 +2,7 @@
 #define CODE_GENERTOR_HPP
 
 #include "NodeAnalyser.hpp"
+#define DEBUG 0
 
 class Generator {
 private:
@@ -12,11 +13,210 @@ public:
     {
         root = node;
         out = ofstream(path);
-        
     }
 
     void generate() {
         
+    }
+
+    /*
+    Program: 
+      ExtDefList 
+    */
+    void analyzeProgram(TreeNode *node)
+    {
+        if (DEBUG)
+            cout << "Program" << endl;
+
+        analyzeExtDefList(node->child[0]);
+    }
+
+    /*
+    ExtDefList: 
+      ExtDef ExtDefList
+    | %empty
+    */
+    void analyzeExtDefList(TreeNode *node)
+    {
+
+    }
+
+    /*
+    ExtDef: 
+      Specifier ExtDecList SEMI
+    | Specifier SEMI
+    | Specifier FunDec CompSt
+    */
+    void analyzeExtDef(TreeNode *node)
+    {
+
+    }
+
+    /*
+    ExtDecList: 
+      VarDec
+    | VarDec COMMA ExtDecList
+    */
+    void analyzeExtDecList(TreeNode *node)
+    {
+    }
+
+    /*
+    Specifier: 
+      TYPE
+    | StructSpecifier
+    */
+    void analyzeSpecifier(TreeNode *node)
+    {
+    }
+
+    /*
+    StructSpecifier: 
+      STRUCT ID LC DefList RC
+    | STRUCT ID
+    */
+    void analyzeStructSpecifier(TreeNode *node)
+    {
+    }
+
+    /*
+    VarDec:
+      ID
+    | VarDec LB INT RB
+    */
+    void analyzeVarDec(TreeNode *node)
+    {
+    }
+
+    /*
+    FunDec: 
+      ID LP VarList RP
+    | ID LP RP
+    */
+    void analyzeFunDec(TreeNode *node)
+    {
+    }
+
+    /*
+    VarList: 
+      ParamDec COMMA VarList
+    | ParamDec
+    */
+    void analyzeVarList(TreeNode *node)
+    {
+    }
+
+    /*
+    ParamDec: 
+      Specifier VarDec
+    */
+    void analyzeParamDec(TreeNode *node)
+    {
+    }
+
+    /*
+    CompSt: 
+      LC DefList StmtList RC
+    */
+    void analyzeCompSt(TreeNode *node)
+    {
+    }
+
+    /*
+    StmtList: 
+      Stmt StmtList
+    | %empty
+    */
+    void analyzeStmtList(TreeNode *node)
+    {
+    }
+
+    /*
+    Stmt: 
+      Exp SEMI
+    | CompSt
+    | RETURN Exp SEMI
+    | IF LP Exp RP Stmt
+    | IF LP Exp RP Stmt ELSE Stmt
+    | WHILE LP Exp RP Stmt
+    */
+    void analyzeStmt(TreeNode *node)
+    {
+    }
+
+    /*
+    DefList: 
+      Def DefList
+    | %empty
+    */
+    void analyzeDefList(TreeNode *node)
+    {
+    }
+
+    /*
+    Def: 
+      Specifier DecList SEMI 
+    */
+    void analyzeDef(TreeNode *node)
+    {
+    }
+
+    /*
+    DecList: 
+      Dec
+    | Dec COMMA DecList
+    */
+    void analyzeDecList(TreeNode *node)
+    {
+    }
+
+    /*
+    Dec: 
+      VarDec
+    | VarDec ASSIGN Exp
+    */
+    void analyzeDec(TreeNode *node)
+    {
+    }
+
+    /*
+    Exp: 
+      Exp ASSIGN Exp                type check
+    | Exp AND Exp                   int, int -> int
+    | Exp OR Exp                    int, int -> int
+    | Exp LT Exp                    int, int -> int
+    | Exp LE Exp                    int, int -> int
+    | Exp GT Exp                    int, int -> int
+    | Exp GE Exp                    int, int -> int
+    | Exp NE Exp                    int, int -> int
+    | Exp EQ Exp                    int, int -> int
+    | Exp PLUS Exp                  T, T -> T
+    | Exp MINUS Exp                 T, T -> T
+    | Exp MUL Exp                   T, T -> T
+    | Exp DIV Exp                   T, T -> T
+    | LP Exp RP                     T -> T
+    | MINUS Exp                     T -> -T
+    | NOT Exp                       int -> int
+    | ID LP Args RP                 func
+    | ID LP RP
+    | Exp LB Exp RB
+    | Exp DOT ID
+    | ID
+    | INT
+    | FLOAT
+    | CHAR
+    */
+    void analyzeExp(TreeNode *node)
+    {
+    }
+
+    /*
+    Args: 
+      Exp COMMA Args
+    | Exp
+    */
+    void analyzeArgs(TreeNode *node)
+    {
     }
 };
 
